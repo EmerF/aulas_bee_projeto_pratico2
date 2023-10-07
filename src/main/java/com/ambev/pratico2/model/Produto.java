6cfb01341f478fa1d7d2fcc8df2afcc43f75cd4b
GRACIELE FERREIRA <graciele.ferreira@ab-inbev.com>
2023-10-07 17:43:44 -0300

    feat: Heranca e Polimorfismo

--- src/main/java/com/ambev/pratico2/model/Produto.java
+++ src/main/java/com/ambev/pratico2/model/Produto.java
@@ -1,4 +1,39 @@
 package com.ambev.pratico2.model;
 
 public class Produto {
+
+    private Long id;
+    private String descricao;
+    private double preco;
+
+    public Produto(Long id, String descricao, double preco) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
+
