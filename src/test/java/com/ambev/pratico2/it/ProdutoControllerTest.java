af6cc77820190ce28dc1b083ed121125a7af4e27
daiane.goncalves <daiane.goncalves@ab-inbev.com>
2023-10-07 18:11:48 -0300

    entrega projeto 2 Daiane

--- src/test/java/com/ambev/pratico2/it/ProdutoControllerTest.java
+++ src/test/java/com/ambev/pratico2/it/ProdutoControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class ProdutoControllerTest {
