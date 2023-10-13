eb9b45771708c510a929db70c57cdde2568af908
Samuel123n <souzasamuka2017@gmail.com>
2023-10-13 02:35:45 -0300

    Atividade 2

---
+++ src/main/java/service/ProdutoServiceImpl.java
@@ -0,0 +1,50 @@
+package service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class ProdutoServiceImpl implements ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    @Autowired
+    public ProdutoServiceImpl(ProdutoRepository produtoRepository) {
+        this.produtoRepository = produtoRepository;
+    }
+
+    @Override
+    public Produto salvarProduto(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    @Override
+    public List<Produto> listarProdutos() {
+        return produtoRepository.findAll();
+    }
+
+    @Override
+    public Produto buscarProdutoPorId(Long id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deletarProduto(Long id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @Override
+    public double calcularAjudaCusto(Produto produto) {
+        // Implemente a lógica de cálculo da ajuda de custo
+        // Exemplo: 5% sobre o preço
+        return produto.getPreco() * 0.05;
+    }
+
+    @Override
+    public double calcularImpostoRenda(Produto produto) {
+        // Implemente a lógica de cálculo do imposto de renda
+        // Exemplo: 10% sobre o preço
+        return produto.getPreco() * 0.10;
+    }
+}
